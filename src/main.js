3f27ed768baec3ed72e8cce062080538bae63ae6
asusdion <dr.dtdavan@gmail.com>
2019-01-30 21:59:06 +0200

    firebaseservice

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import { FIREBASE_CONFIG } from './config';
-const fb = firebase.initializeApp(FIREBASE_CONFIG);
---
+++ src/services/fbS.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { FIREBASE_CONFIG } from './config';
+const fb = firebase.initializeApp(FIREBASE_CONFIG);
+
+export class FirebaseService{
+    constructor(){}
+}
